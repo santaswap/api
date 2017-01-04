adaeb7e82b2ee2553fa54784a34fa7d43d3326b6
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-01-04 13:26:50 -0500

    initial commit

---
+++ users/getByGroup.js
@@ -0,0 +1,37 @@
+'use strict';
+const AWS = require('aws-sdk');
+const docs = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10'
+});
+
+module.exports.get = (event, context, callback) => {
+    console.log('Getting users by group');
+
+    let groupId = event.pathParameters.groupId;
+    
+    getUsersByGroup(groupId)
+        .then( users => {
+            console.log('Got users', users);
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(users.Items)
+        };
+        callback(null, response);
+    })
+    .catch( err => {
+        console.log('Error getting users', err);
+        callback('Error');
+    });
+};
+
+var getUsersByGroup = (groupId) => {
+    var params = {
+        TableName: process.env.TABLE_NAME,
+        KeyConditionExpression: 'HashKey = :groupId',
+        ExpressionAttributeValues: {
+            ":groupId": groupId
+        }
+    };
+    console.log('Getting all users with params', params);
+    return docs.scan(params).promise();
+};
---
+++ users/list.js
@@ -0,0 +1,31 @@
+'use strict';
+const AWS = require('aws-sdk');
+const docs = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10'
+});
+
+module.exports.list = (event, context, callback) => {
+    console.log('Getting all users');
+    
+    getUsers()
+        .then( users => {
+            console.log('Got users', users);
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(users.Items)
+        };
+        callback(null, response);
+    })
+    .catch( err => {
+        console.log('Error getting users', err);
+        callback('Error');
+    });
+};
+
+var getUsers = () => {
+    var params = {
+        TableName: process.env.TABLE_NAME
+    };
+    console.log('Getting all users with params', params);
+    return docs.scan(params).promise();
+};
