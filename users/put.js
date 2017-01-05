e5a14d125d30dae4c5db284f8ea1a2c40c87685a
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-01-04 20:36:58 -0500

    initial commit of update user

---
+++ users/put.js
@@ -0,0 +1,84 @@
+'use strict';
+const AWS = require('aws-sdk');
+const docs = new AWS.DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10'
+});
+
+module.exports.put = (event, context, callback) => {
+
+  let data = JSON.parse(event.body);
+  console.log('Updating user', data);
+  
+  var getUser = (data) => {
+    return new Promise( (resolve, reject) => {
+      let timestamp = new Date().getTime();
+
+      resolve({
+        groupId: event.pathParameters.groupId,
+        userId: event.pathParameters.userId,
+        bio: data.bio,
+        address: data.address,
+        updatedAt: timestamp
+      });
+    });
+  }
+  
+  var updateUser = (user) => {
+
+    let expression = getExpression(user);
+    var params = {
+      TableName: process.env.TABLE_NAME,
+      Key: { 
+          userId: user.id,
+          groupId: group.id
+      },
+      UpdateExpression: expression.updateExpression,
+      ExpressionAttributeNames: expression.expressionAttributeNames,
+      ExpressionAttributeValues: expression.expressionAttributeValues
+    };
+    console.log('Updating user with params', params);
+    return new Promise( (resolve, reject) => {
+      docs.update(params, err => err ? reject(err) : resolve(user) );
+    });
+  };
+
+  var getExpression = (user) => {
+    let updateExpression = 'set ';
+    let expressionAttributeNames = {};
+    let expressionAttributeValues = {};
+    if(user.name) {
+      updateExpression += 'bio = :bio';
+      expressionAttributeValues[':bio'] = user.bio; 
+    }
+    if(user.rules) {
+      updateExpression ? updateExpression += ', ' : updateExpression += ' ';
+      updateExpression += 'address = :address';
+      expressionAttributeValues[':address'] = user.address; 
+    }
+    if(user.updatedAt) {
+      updateExpression ? updateExpression += ', ' : updateExpression += ' ';
+      updateExpression += 'updatedAt = :updatedAt';
+      expressionAttributeValues[':updatedAt'] = user.updatedAt; 
+    }
+    return {
+      updateExpression: updateExpression,
+      expressionAttributeNames: expressionAttributeNames,
+      expressionAttributeValues: expressionAttributeValues
+    };
+  };
+
+  getUser(data)
+    .then(updateUser)
+    .then( user => {
+      console.log('Updated user', user);
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(user)
+      };
+      callback(null, response);
+    })
+    .catch( err => {
+      console.log('Error updating user', err);
+      callback('Error');
+    });
+};
