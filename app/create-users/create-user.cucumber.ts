037ba39ad4e35b4bcbbb45b51584ce851435e01f
Phillip <phillip.manwaring@gmail.com>
2019-01-02 01:12:08 -0500

    add tests

--- app/create-users/create-user.cucumber.ts
+++ app/create-users/create-user.cucumber.ts
@@ -1,3 +1,30 @@
 import { binding, given, when, then, after } from 'cucumber-tsflow';
 import { expect } from 'chai';
-// import { getDeployedUrl } from ''
+import { post } from 'request-promise';
+import { getDeployedUrl } from '@manwaring/serverless-test-helper';
+
+const URL = getDeployedUrl();
+
+@binding()
+export class CreateUser {
+  userRequest = { name: 'This is my name' };
+  userResponse: any;
+
+  @when(/a valid user create request is made/)
+  public async createUser() {
+    const params = {
+      url: `${URL}/users`,
+      method: 'post',
+      simple: false,
+      body: JSON.stringify(this.userRequest)
+    };
+    this.userResponse = JSON.parse(await post(params));
+  }
+
+  @then(/the API response will include the new user/)
+  public validateUser() {
+    expect(this.userResponse).to.not.equal(undefined);
+    expect(this.userResponse.userId).to.not.equal(undefined);
+    expect(this.userResponse.name).to.equal(this.userRequest.name);
+  }
+}
--- app/create-users/create-user.feature
+++ app/create-users/create-user.feature
@@ -1,6 +1,5 @@
 Feature: User management
 
-  Scenario Outline: Create new user
+  Scenario: Create new user
     When a valid user create request is made
-    Then the API response will be successful
     And the API response will include the new user
